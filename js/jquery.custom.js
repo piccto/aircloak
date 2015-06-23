19b0aab4962c81805bf80db4e02c26ab3ecd4957
piccto <peterwoellgens@Peters-Mac-mini.local>
2015-06-23 08:27:50 +0200

    Korr_23.06

--- js/jquery.custom.js
+++ js/jquery.custom.js
@@ -7,4 +7,19 @@ $(document).ready(function(){
 		$(".nav_responsive").toggleClass("close_menu");
 	});
 	
+	// Scrolling Anker-Link 
+		// Reminder for Dummies: Link <a href="#services">Jump to services</a> / Anker <div id="services"></div>
+		$('a[href^="#"]').on('click', function(e) {
+			e.preventDefault();
+	
+			var target = this.hash,
+				$target = $(target);
+	
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top
+			}, 600, 'swing', function() {
+				window.location.hash = target;
+			});
+		});
+		
 });
